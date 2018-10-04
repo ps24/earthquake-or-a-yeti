70050e9f5493319fedd96d32c33ce752c0d1e63c
sabliao <sabrinagliao@gmail.com>
2018-10-03 20:48:33 -0700

    Add progress bar to loading screen

--- components/Loading/Loading.js
+++ components/Loading/Loading.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import { Image, ImageBackground, StatusBar, Text, View } from 'react-native';
+import {
+  Image,
+  ImageBackground,
+  Platform,
+  ProgressBarAndroid,
+  ProgressViewIOS,
+  StatusBar,
+  Text,
+  View,
+} from 'react-native';
 
 import loadingStyles from './loadingStyles';
+
+const UPDATE_INTERVAL = 1000; // 1 sec
 
 class Loading extends React.Component {
   static navigationOptions = {
@@ -11,16 +22,31 @@ class Loading extends React.Component {
     super(props);
     this.state = {
       loadingBackground: require('../images/radar_200.png'),
-      loadingTopImage: require('../images/ripple_200.gif')
+      loadingTopImage: require('../images/ripple_200.gif'),
+      progress: 0,
+      timePassed: 0,
     };
+    this.checkProgress = this.checkProgress.bind(this);
   }
 
   componentDidMount() {
+    this.next = setInterval(this.checkProgress, UPDATE_INTERVAL);
+  }
+
+  checkProgress() {
     const { navigate } = this.props.navigation;
     const monsterIndex = this.props.navigation.getParam('monsterIndex', 0);
-    this.next = setInterval(() => {
+    const totalTime = 5000; // we want to transition (faking our api call) after 5 sec
+    const timePassed = this.state.timePassed + UPDATE_INTERVAL;
+    const progress = timePassed / totalTime;
+    this.setState({
+      timePassed,
+      progress,
+    });
+    if (timePassed == totalTime) {
+      clearInterval(this.next); // only way to reliably clear interval before navigate away
       navigate('Monster', { monsterIndex: monsterIndex });
-    }, 5000);
+    }
   }
 
   componentWillUnmount() {
@@ -47,6 +73,8 @@ class Loading extends React.Component {
               resizeMode='contain'
             />
           </ImageBackground>
+          {Platform.OS == 'android' && <ProgressBarAndroid progress={this.state.progress} styleAttr="Horizontal" />}
+          {Platform.OS == 'ios' && <ProgressViewIOS progress={this.state.progress} progressViewStyle="bar" />}
         </View>
       </View>
     );
